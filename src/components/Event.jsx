7919f137ee46d8e74562b56f373772bc1c7f2a3a
Uday <udayrajveerjain@gmail.com>
2024-12-03 19:41:23 +1300

    Added Shallow Implementation for user to be able to join events

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-
+import { useState } from 'react';
 const event = ({ eventName, isLoggedIn }) => {
+  const [isJoined, setIsJoined] = useState(false);
+  const handleButtonClick = () => {
+    setIsJoined(true);
+  };
   if (!isLoggedIn) {
     return <div>{eventName}</div>;
   } else {
     return (
       <div>
-        {eventName} <button>Join</button>
+        {eventName}{' '}
+        <button onClick={handleButtonClick}>
+          {isJoined ? 'Joined!' : 'Join'}
+        </button>
       </div>
     );
   }
