928df3fe475c084e8227c9a63201e7e00ad9f30a
Uday <udayrajveerjain@gmail.com>
2024-12-10 16:22:23 +1300

    Added Express.static

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const errorHandler = require('./middleware/errorHandler');
 const app = express();
 app.use(cors());
+app.use(express.static('dist'));
 const port = 3000;
 
 let events = [
