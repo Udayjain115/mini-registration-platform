099674a6ca1bbb7f440338ad7986061402cd9652
Uday <udayrajveerjain@gmail.com>
2024-12-10 16:18:28 +1300

    added dist to back end

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3000/api/events';
+const baseUrl = '/api/events';
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:3000/api/users';
+const baseUrl = '/api/users';
 
 const getAll = () => {
   return axios.get(baseUrl).then((response) => response.data);
