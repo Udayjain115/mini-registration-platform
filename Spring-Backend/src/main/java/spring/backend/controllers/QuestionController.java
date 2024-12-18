129d7379b90b538e5df93eb31dfe95511f6c1bc6
Uday <udayrajveerjain@gmail.com>
2024-12-18 18:09:01 +1300

    Created QuestionController

---
+++ Spring-Backend/src/main/java/spring/backend/controllers/QuestionController.java
@@ -0,0 +1,34 @@
+package spring.backend.controllers;
+
+import jakarta.validation.Valid;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import spring.backend.models.Question;
+import spring.backend.services.QuestionService;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/questions")
+public class QuestionController {
+  @Autowired QuestionService questionService;
+
+  @GetMapping
+  public ResponseEntity<List<Question>> getQuestions() {
+    List<Question> questions = questionService.getAllQuestions();
+    return ResponseEntity.ok(questions);
+  }
+
+  @PostMapping
+  public ResponseEntity<Question> createQuestion(@Valid @RequestBody Question question) {
+    Question createdQuestion = questionService.createQuestion(question);
+
+    return ResponseEntity.ok(createdQuestion);
+  }
+}
