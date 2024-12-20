6e6b5eacf60b2879c6a0aee199b3409e792793fe
Uday <udayrajveerjain@gmail.com>
2024-12-20 14:11:26 +1300

    Added a get mapping to get single question

--- Spring-Backend/src/main/java/spring/backend/controllers/QuestionController.java
+++ Spring-Backend/src/main/java/spring/backend/controllers/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,5 +31,12 @@ public class QuestionController {
     Question createdQuestion = questionService.createQuestion(question);
 
     return ResponseEntity.ok(createdQuestion);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Question> getQuestionByTitle(@PathVariable String id) {
+    Question question = questionService.getQuestionByTitle(id);
+
+    return ResponseEntity.ok(question);
   }
 }
--- Spring-Backend/src/main/java/spring/backend/services/QuestionService.java
+++ Spring-Backend/src/main/java/spring/backend/services/QuestionService.java
@@ -2,6 +2,7 @@ package spring.backend.services;
 
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,5 +27,15 @@ public class QuestionService {
               }
             });
     return questionRepository.save(question);
+  }
+
+  public Question getQuestionByTitle(String title) {
+    Optional<Question> question = questionRepository.findById(title);
+
+    if (question.isPresent()) {
+      return question.get();
+    } else {
+      throw new IllegalArgumentException("Question Not Found");
+    }
   }
 }
