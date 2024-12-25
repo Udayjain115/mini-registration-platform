6504e46d207707d21b43e9d6dad85112c00ae897
Uday <udayrajveerjain@gmail.com>
2024-12-25 17:11:51 +1300

    added frontend Attempt service

---
+++ Frontend/src/services/attemptService.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+const baseUrl = '/api/attempts';
+
+const getAll = () => {
+  return axios.get(baseUrl).then((response) => response.data);
+};
+
+const getOne = (id) => {
+  return axios.get(`${baseUrl}/${id}`).then((response) => response.data);
+};
+
+const create = (newObject) => {
+  return axios.post(baseUrl, newObject).then((response) => response.data);
+};
+
+const update = (id, newObject) => {
+  console.log('update', newObject);
+  return axios
+    .put(`${baseUrl}/${id}`, newObject)
+    .then((response) => response.data)
+    .catch((error) => {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+    });
+};
+
+const remove = (id) => {
+  return axios.delete(`${baseUrl}/${id}`).then((response) => response.data);
+};
+
+export default { getAll, getOne, create, update, remove };
